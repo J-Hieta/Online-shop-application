3ccd8e89e6833615275eaa9e4881cc1fd335cb98
Atte <t6kaat01@students.oamk.fi>
2018-11-20 14:02:17 +0000

    User info
    Fetch logged user's data from database.
    Default profile picture.

---
+++ Scripts/getUserInfo.php
@@ -0,0 +1,14 @@
+<?php 
+    include_once 'connection.php';
+    
+//     Use in final version
+//     $userInfo = $conn->query("SELECT * FROM users WHERE email = '".$_SESSION['email']."'");
+//     For testing
+    $userInfo = $conn->query("SELECT * FROM users WHERE email = 'test@email.com'");
+    foreach ($userInfo as $column) {
+        $first_name = $column['first_name'];
+        $last_name = $column['last_name'];
+        $email = $column['email'];
+        $dob = $column['date_of_birth'];
+    }
+?>
