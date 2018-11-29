ab85f1a6d28c6ee5bb72e7c3a8961026749faea0
Jussi Hieta <t6hiju02@students.oamk.fi>
2018-11-29 14:04:50 +0000

    cursor : pointer

--- Layouts/navbar.php
+++ Layouts/navbar.php
@@ -12,7 +12,7 @@
       <div class="collapse navbar-collapse" id="myNavbar">
         <ul class="nav navbar-nav">
           <li class="active"><a href="index.php">Home</a></li>
-          <li><span id="items_in_cart"></span> <a href="../Layouts/cart.php" ><span class="glyphicon glyphicon-shopping-cart"></span> Cart</a></li>
+          <li><span id="items_in_cart"></span> <a href="../Layouts/cart.php" style="cursor: pointer" ><span class="glyphicon glyphicon-shopping-cart"></span> Cart</a></li>
         </ul>
         <ul class="nav navbar-nav navbar-right">
           
